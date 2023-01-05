45b463e4cb663a2558f883dd7dfc59603b2832e6
KatSchultz <ks.katieschultz@gmail.com>
2023-01-05 16:40:33 -0500

    map showing up

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useMemo } from "react";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
+import "../styles/global.css";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type DirectionsResult = google.maps.DirectionsResult;
+type MapOptions = google.maps.MapOptions;
 
 export default function Map() {
-  return <div>Map</div>;
+  //specifying center with useMemo will prevent re-loading of map on center
+  const center = useMemo(() => ({ lat: 43, lng: -80 }), []);
+
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Commute?</h1>
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+        ></GoogleMap>
+      </div>
+    </div>
+  );
 }
